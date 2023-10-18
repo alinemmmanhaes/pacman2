422530bcc719d51d2812eb3dd79a6915be6e5d4b
Aline <142928899+alinemmmanhaes@users.noreply.github.com>
2023-10-17 23:27:53 -0300

    Add files via upload

---
+++ Respostas/Aline/tPosicao.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "tPosicao.h"
+
+tPosicao* CriaPosicao(int linha, int coluna){
+    tPosicao * posicao = (tPosicao*) malloc(sizeof(tPosicao));
+
+    posicao->coluna = coluna;
+    posicao->linha = linha;
+
+    return posicao;
+}
+
+tPosicao* ClonaPosicao(tPosicao* posicao){
+    tPosicao * posicao2 = malloc(sizeof(tPosicao));
+    *posicao2 = *posicao;
+    return posicao2;
+}
+
+int ObtemLinhaPosicao(tPosicao* posicao){
+    return posicao->linha;
+}
+
+int ObtemColunaPosicao(tPosicao* posicao){
+    return posicao->coluna;
+}
+
+void AtualizaPosicao(tPosicao* posicaoAtual, tPosicao* posicaoNova){
+    *posicaoAtual = *posicaoNova;
+}
+
+bool SaoIguaisPosicao(tPosicao* posicao1, tPosicao* posicao2){
+    if(posicao1->coluna == posicao2->coluna && posicao1->linha == posicao2->coluna){
+        return true;
+    }
+    return false;
+}
+
+void DesalocaPosicao(tPosicao* posicao){
+    free(posicao);
+}
